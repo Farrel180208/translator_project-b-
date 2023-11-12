1ec6480546b318b3a4813737f3897357cfaf8b2f
Jonathan Farrel <jonathanfarrel@students.ignatiusglobal.sch.id>
2023-11-12 11:46:13 +0700

    third commit

--- translator.py
+++ translator.py
@@ -57,3 +57,32 @@ def translate():
         'pt': 'Portugis',
         'ja': 'Jepang',
     }
+
+    destination_choices = request.form.getlist('destination')
+    translations = []
+
+    for choice in destination_choices:
+        if choice not in lang_destinations:
+            return render_template('error.html', message=f"Pilihan bahasa tujuan '{choice}' tidak valid. Silakan coba lagi.")
+
+        dest_lang = lang_destinations[choice]
+        translated_text = translator.translate(text, src='id', dest=dest_lang)
+        translations.append((dest_lang, translated_text.text))
+
+        # Tambahkan terjemahan ke riwayat
+        translation_history.append((text, translated_text.text, dest_lang))
+
+    # Tambahkan terjemahan ke bahasa Inggris jika dipilih
+    if 'en' in destination_choices:
+        english_translation = translator.translate(text, src='id', dest='en')
+        translations.append(('en', english_translation.text))
+        translation_history.append((text, english_translation.text, 'en'))
+
+    return render_template('result.html', text=text, translations=translations, lang_descriptions=lang_descriptions)
+
+@app.route('/history')
+def history():
+    return render_template('history.html', translation_history=translation_history)
+
+if __name__ == "__main__":
+    app.run(debug=True)
